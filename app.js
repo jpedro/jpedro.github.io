5ed0a67b195f7a68f7a312edcccd3c6f30663515
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-03-16 23:25:04 +0100

    sdfg

---
+++ app.js
@@ -0,0 +1,6 @@
+const body = document.body;
+const comments = document.getElementById("comments");
+window.addEventListener("load", (event) => {
+    body.style.backgroundColor = "#eee";
+    comments.innerHTML = "<ul><li>Comment 1</li></ul>";
+});
