25b1e2148dbb64a60c997a156ffc72cf44a86793
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-03-19 00:37:59 +0100

    fixed some minor stuff, might need some additional work.

--- bin/index.py
+++ bin/index.py
@@ -26,7 +26,7 @@ TEMPLATE_INDEX = """
 
 <div id="comments" data-added="manually"></div>
 <script src="https://jpedro.github.io/js/v1/comments.js"></script>
-<script defer>Comments.mount(document.body.children[0]);</script>
+<script>Comments.mount(document.body.children[0]);</script>
 """
 
 # #### Tags
