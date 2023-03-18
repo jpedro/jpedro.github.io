395859cc98713e6079276306f15870df6302ad00
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-03-18 22:58:30 +0100

    Does not work.

--- bin/index.py
+++ bin/index.py
@@ -20,11 +20,12 @@ TEMPLATE_INDEX = """
 
 {{ pages }}
 
-
  &nbsp;
 
 Made with some <3 [Not a lot](https://github.com/jpedro/jpedro.github.io)
 <!-- This ~~will be eventually~~ is generated. -->
+
+ &nbsp;
 
 <div id="comments" data-added="manually"></div>
 <script src="/static/js/comments.js"></script>
