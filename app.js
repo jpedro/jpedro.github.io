21a40be3208e42026ffd57db15dc9c886d222537
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-03-18 12:59:06 +0100

    go

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const body = document.body;
 const comments = document.getElementById("comments");
+
+const addComment = (content) => {
+    const el = document.createElement("li")
+    li.innerHTML = content;
+    return li;
+};
+
+const addComments = () => {
+    const total = Math.random() * (10 - 1) + 1;
+    const ul = document.createElement("ul")
+    comments.appendChild(ul);
+    for (i = 0; i < total; i++) {
+        const li = addComment("This is generated comment " + i);
+        ul.appendChild(li);
+    }
+};
+
+
 window.addEventListener("load", (event) => {
     body.style.backgroundColor = "#eee";
-    comments.innerHTML = "<ul><li>Comment 1</li></ul>";
+    addComments();
 });
