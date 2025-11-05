42e84479b4987abf78a2640753545b10ce81303e
Pedro Barbosa <jpedro.barbosa@gmail.com>
2025-11-05 23:57:12 +0100

    jobs... steve jobs

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    /// Directory to load files from
+    /// Directory to load posts from
     #[arg(short, long, default_value = ".posts")]
     pub dir: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::path::Path;
-// use std::path::PathBuf;
+
 use clap::Parser;
 
 mod args;
